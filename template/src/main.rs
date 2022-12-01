ae19df226b08710b474f3f44d83fead606fba870
Johan Kamb <johan.kamb@dice.se>
2022-12-01 11:09:26 +0100

    Setup

---
+++ template/src/main.rs
@@ -0,0 +1,40 @@
+use anyhow::Result;
+
+type Input = [u32];
+
+fn parse(input: &str) -> Result<Vec<u32>> {
+    todo!()
+}
+
+fn part1(input: &Input) -> Result<u32> {
+    todo!()
+}
+
+fn part2(input: &Input) -> Result<u32> {
+    todo!()
+}
+
+fn main() -> Result<()> {
+    let input = include_str!("input.txt");
+    let input = parse(input)?;
+    println!("{}", part1(&input)?);
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    const INPUT: &str = include_str!("input_test.txt");
+
+    #[test]
+    fn test_part1() {
+        let res = part1(&parse(INPUT).unwrap()).unwrap();
+        assert_eq!(res, 7)
+    }
+
+    #[test]
+    fn test_part2() {
+        let res = part2(&parse(INPUT).unwrap()).unwrap();
+        assert_eq!(res, 5)
+    }
+}
